ef4a2d3e103ef8b4290c95d8941a55b8eca6ff42
Lebroncorp <lebroncorp@yahoo.com>
2024-04-12 14:08:15 +0000

    promise debug

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,7 +3,6 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   /**
-  * allSettled creates a Promise that is resolved with an array of results
   * when all of the provided Promises resolve or reject.
   * Takes in an array of Promises.
   * Returns a new Promise.
@@ -11,8 +10,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     // Call the signUpUser() and uploadPhoto() functions
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    // When the promises are all settled it should return an array with the
-    // given structure
+    // When promises are all settled it should return array wit structures
     .then((results) => (
       results.map((out) => ({
         status: out.status,
