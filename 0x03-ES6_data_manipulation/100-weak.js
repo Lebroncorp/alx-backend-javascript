78bd81193696a9551f57a4119c36ab008abf5163
Lebroncorp <lebroncorp@yahoo.com>
2024-04-17 01:16:53 +0000

    weakish

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,26 @@
+export const weakMap = new WeakMap();
+
+/**
+ * The maximum number of calls for an endpoint.
+ */
+const MAX_ENDPOINT_CALLS = 5;
+
+/**
+ * Queries the API with the given endpoint and keeps track of the number
+ * of times the endpoint has been called.
+ *
+ * @param {string} endpoint - The endpoint to query
+ */
+export function queryAPI(endpoint) {
+  // If the endpoint has not been called before,
+  // set its value in the weakMap to 0
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  // Increases the number of times the endpoint has been called
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  // If endpoint has been called too many times, throw an error
+  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
+    throw new Error('Endpoint load is high');
+  }
+}
