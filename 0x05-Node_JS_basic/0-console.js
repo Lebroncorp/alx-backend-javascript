b7a5ba79f64198ff44cfb88cde300b9a7ce3f078
Lebroncorp <lebroncorp@yahoo.com>
2024-06-04 08:08:03 +0000

    all tasks

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * displays a msg on stdout
+ * @param {String} msg to be displayed
+ * @author Kenechukwu Uzodinma <https://github.com/LebronCorp>
+ */
+
+const displayMessage= (msg) => {
+  console.log(msg);
+}
+
+module.exports = displayMessage;
